03cdaac5c4a9807acee19e920f368cf74223e8f6
Jeff Minbiole <minbiole.jeff@gmail.com>
2016-08-19 19:20:11 -0500

    working on the JavaScript

---
+++ src/character-list.js
@@ -0,0 +1,27 @@
+'use strict';
+@import Character from 'character';
+
+export default class CharacterList {
+  constructor(element, data) {
+    this.element = element.querySelector('.charTiles');
+    this.data = data.data;
+  }
+  render() {
+    this.element.innerHTML = '';
+
+    this.data.results.forEach((characters) => {
+      var frame = document.createElement('div');
+      frame.classList = 'frame__small';
+      var img = document.createElement('img');
+      img.classList = 'frame-img';
+      img.src = characters.thumbnail.path + '.jpg';
+
+      var title = document.createElement('p');
+      title.classList = 'charName';
+
+      var character = new Character(charData);
+      character.render(charData);
+
+  });
+}
+}
